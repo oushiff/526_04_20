ba7b5ad7669eb34b263f6b69450dd543c62f1cf8
FranzFei <ffinuyasha@sina.com>
2016-04-20 17:26:38 -0700

    initial from Yutian

---
+++ assets/scripts/Singleton/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Singleton<GameManager> {
+
+	private float _timeRemaining;
+
+	public float TimeRemaining 
+	{
+		get { return _timeRemaining; }
+		set { _timeRemaining = value; }
+	}
+
+	private int _numCoins;
+
+	public int NumCoins {
+		get { return _numCoins; }
+		set { _numCoins = value; }
+	}
+
+    private float _timeConsumed;
+
+    public float TimeConsumed
+    {
+        get { return _timeConsumed; }
+        set { _timeConsumed = value; }
+    }
+
+    private int _coinsGot;
+
+    public int CoinsGot
+    {
+        get { return _coinsGot; }
+        set { _coinsGot = value; }
+    }
+
+	private float _health;
+
+	public float Health
+	{
+		get { return _health; }
+		set { _health = value; }
+	}
+
+	private float maxTime = 60; // In seconds.
+
+	private float maxHealth = 100; // The max health of the hero
+
+	// Use this for initialization
+	void Start () {
+		TimeRemaining = maxTime;
+		Health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TimeRemaining -= Time.deltaTime;
+
+		if (TimeRemaining <= 0) {
+			RestartGame ();
+		}
+	}
+
+
+	public void RestartGame(){
+		//Debug.LogError ("Reset");
+
+
+		Application.LoadLevel (Application.loadedLevel);
+		TimeRemaining = maxTime;
+		NumCoins = 0;
+		//Reset the health of the hero
+		Health = maxHealth;
+
+	}
+}
---
+++ assets/scripts/coins and enemy/Enemy/Enemy.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Character {
+
+
+	private IEnemyState currentState;
+	 
+	public GameObject target{get; set;} 
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+
+		ChangeState (new IdleState ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentState.Execute ();
+
+		LookAtTarget ();
+	
+	}
+
+	private void LookAtTarget(){
+		if (target != null) {
+			float xDir = target.transform.position.x - transform.position.x;
+
+			if (xDir < 0 && facingRight || xDir > 0 && !facingRight) {
+				ChangeDirection ();
+			}
+		}
+	}
+
+
+	public void ChangeState(IEnemyState newState){
+		if (currentState != null) { //If there already exist a state, exit it. 
+			currentState.Exit ();
+		}
+		currentState = newState;
+		currentState.Enter (this);
+	}
+
+	public void Move(){
+		MyAnimator.SetFloat ("speed", 1);
+
+		transform.Translate (GetDirection () * movementSpeed * Time.deltaTime);
+	}
+
+	public Vector2 GetDirection(){
+
+		return facingRight ? Vector2.right : Vector2.left;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			Debug.Log ("Player's health is deducted");
+			GameManager.Instance.Health -= 50;
+			Debug.Log ("Current Health:" + GameManager.Instance.Health);
+			if (GameManager.Instance.Health <= 0) {
+				GameManager.Instance.RestartGame ();
+			}
+		}
+			
+		else currentState.OnTriggerEnter(other);
+	}
+
+	[SerializeField]
+	private GameObject enemySelf;
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			Debug.Log ("Enemy died");
+			Destroy (enemySelf); // the health is deducted, and enemy died
+		}
+	}
+
+}
