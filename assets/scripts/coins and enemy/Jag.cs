dc3b8a24afd4247d57ca7df7911c2428d673bff0
Xin Ge <xinge@gexindeMacBook-Pro.local>
2016-04-21 12:33:16 -0700

    Add jag.cs so that environment jag will decrease the plyer's health

---
+++ assets/scripts/coins and enemy/Jag.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jag : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider2d)
+	{
+		if (collider2d.gameObject.tag == "PlayerSnow" || collider2d.gameObject.tag == "PlayerCloud")
+		{
+			GetAttacked();
+		}
+	}
+
+
+	[SerializeField]
+	private float healthDecrease = 20;
+
+	private void GetAttacked()
+	{
+		GameManager.Instance.Health += healthDecrease;
+		if (GameManager.Instance.Health <= 0) {
+			GameManager.Instance.RestartGame ();
+		}
+
+	}
+}
