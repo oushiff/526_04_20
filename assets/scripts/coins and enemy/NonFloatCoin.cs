da8959cdb16d0caec98e940ecfa7345f6fa487b7
Xin Ge <xinge@gexindeMacBook-Pro.local>
2016-04-21 11:01:35 -0700

    Add a new kind of coins that will attached to wheels and circles that can move with them

---
+++ assets/scripts/coins and enemy/NonFloatCoin.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NonFloatCoin : MonoBehaviour {
+	[SerializeField]
+	private float rotateSpeed = 1.0f;
+
+	[SerializeField]
+	private float floatSpeed = 0.5f; // In cycles (up and down) per second
+
+	[SerializeField]
+	private float movementDistance = 0.5f; // The maximum distance the coin can move up and down.
+
+
+	private float startingY;
+	private bool isMovingUp = true;
+
+	void OnTriggerEnter2D(Collider2D collider2d)
+	{
+		if (collider2d.gameObject.tag == "Player")
+		{
+			Pickup();
+		}
+	}
+
+	private void Pickup()
+	{
+		GameManager.Instance.NumCoins++;
+		Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		startingY = transform.position.y;
+		transform.Rotate (transform.up, Random.Range (0f, 360f));
+
+		StartCoroutine (Spin ());
+	}
+
+
+	private IEnumerator Spin()
+	{
+		while (true) 
+		{
+			transform.Rotate (transform.up, 360 * rotateSpeed * Time.deltaTime);
+			yield return 0;
+		}
+	}
+}
